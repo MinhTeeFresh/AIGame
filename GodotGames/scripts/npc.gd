1c65f5010d1701565a8788761cafba72c3625607
Minh Tran <78166240+MinhTeeFresh@users.noreply.github.com>
2024-01-31 18:03:51 -0600

    feat(player input)

--- GodotGames/scripts/npc.gd
+++ GodotGames/scripts/npc.gd
@@ -1,29 +1,11 @@
 extends CharacterBody2D
-
-var motion = Vector2()
-var state = 0
-
-func _physics_process(delta):
-	
-	if state == 0:
-		pass
-	elif state == 1:
-		velocity.x = 100
-	elif state == 2:
-		velocity.x = -100
-	
-	move_and_slide()
+var character = null
 
 func _on_area_2d_body_entered(body):
-	if body.has_method("player"):
+	character = body.get_name()
+	if character == "player":
 		$Label.show()
 
-
 func _on_area_2d_body_exited(body):
-	if body.has_method("player"):
-		$Label.hide()
-
-
-func _on_timer_timeout():
-	state = floor(randf_range(0,3))
-	print(state)
+	character = null
+	$Label.hide()
--- GodotGames/scripts/player.gd
+++ GodotGames/scripts/player.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 const speed = 100
 var current_dir = "none"
 var enemy = null
+var dialog = false
+var playerinput = ""
 
 func _ready():
 	$AnimatedSprite2D.play("cat")
 
 func _physics_process(delta):
-	player_movement(delta)
+	if dialog == false:
+		player_movement(delta)
 	
 func player_movement(delta):
 	
@@ -69,22 +72,27 @@ func play_anim(movement):
 		elif movement == 0:
 			anim.play("back_idle")
 			
-func player():
-	pass
-			
 func _on_area_2d_body_entered(body):
 	enemy = body.get_name()
 	print(enemy)
 
 func _on_area_2d_body_exited(body):
-	if body.has_method("enemy"):
-		enemy = null
+	enemy = null
+	print("left")
 		
 func enemy_inter():
 	pass
 	
 func _input(event):
-	if Input.is_action_pressed('e_key') and enemy != null:
-			#$LineEdit.show()
-			print("pressed E")
-			#get_tree().set_input_as_handled()
+	if Input.is_action_pressed('e_key') and enemy != null and dialog == false:
+		$LineEdit.show()
+		dialog = true
+		print("pressed E")
+
+
+func _on_line_edit_text_submitted(new_text):
+	if !new_text.is_empty():
+		playerinput = new_text
+	dialog = false
+	$LineEdit.hide()
+	$LineEdit.clear()
